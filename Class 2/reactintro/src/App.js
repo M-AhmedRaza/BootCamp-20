8dfdf6d1d9fd0959508cc99764f8920269c5034f
M-AhmedRaza <66229629+M-AhmedRaza@users.noreply.github.com>
2020-06-16 18:56:39 -0700

    Update App.js

--- Class 2/reactintro/src/App.js
+++ Class 2/reactintro/src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World, Wednesday</h1>
+      <h1>Hello World, Wednesday 17th June</h1>
     </div>
   );
 }
